e270eafd43d9511809df33a47b3652b6a6880c26
JzzzzSkr <harisskr@outlook.com>
2023-12-28 19:24:34 +1100

    form alert

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,67 @@
+import "./index.scss";
+import { Card, Form, Input, Button, message } from "antd";
+import logo from "@/assets/logo.png";
+import React, { useState } from "react";
+import { Alert } from "antd";
+import classNames from "classnames";
+
 const Login = () => {
-  return <div>I am Login</div>;
+  const [showAlert, setShowAlert] = useState(false);
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    setShowAlert(true);
+  };
+  
+  return (
+    <div className="login">
+      {showAlert && (
+        <Alert
+          message="登录成功"
+          type="success"
+          className={classNames("alert")}
+        />
+      )}
+
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form validateTrigger="onBlur" onFinish={onFinish}>
+          <Form.Item
+            name="mobile"
+            // 多条校验逻辑 先校验第一条 第一条通过之后再校验第二条
+            rules={[
+              {
+                required: true,
+                message: "请输入手机号",
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "请输入正确的手机号格式",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: "请输入验证码",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
