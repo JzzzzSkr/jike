8b02d9b61ee14cd3e042877cf963c21118658ba5
JzzzzSkr <harisskr@outlook.com>
2024-01-06 19:02:01 +1100

    打包优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,59 @@
-import Login from "@/pages/login";
-import Layout from "@/pages/layout";
 import * as React from "react";
-import AuthRoute from "../components/AuthRouters";
 import { createBrowserRouter } from "react-router-dom";
-import Article from "@/pages/article";
-import Home from "@/pages/home";
-import Publish from "@/pages/publish";
+import AuthRoute from "../components/AuthRouters";
+import { Suspense, lazy } from "react";
+
+// 使用 React.lazy 动态导入页面组件
+const Login = lazy(() => import("@/pages/login"));
+const Layout = lazy(() => import("@/pages/layout"));
+const Article = lazy(() => import("@/pages/article"));
+const Home = lazy(() => import("@/pages/home"));
+const Publish = lazy(() => import("@/pages/publish"));
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <AuthRoute>
-        <Layout />
-      </AuthRoute>
+      <Suspense fallback={<div>Loading...</div>}>
+        <AuthRoute>
+          <Layout />
+        </AuthRoute>
+      </Suspense>
     ),
     children: [
       {
         path: "article",
-        element: <Article />,
+        element: (
+          <Suspense fallback={<div>Loading article...</div>}>
+            <Article />
+          </Suspense>
+        ),
       },
       {
         path: "/",
-        element: <Home />,
+        element: (
+          <Suspense fallback={<div>Loading home...</div>}>
+            <Home />
+          </Suspense>
+        ),
       },
       {
         path: "publish",
-        element: <Publish/>,
+        element: (
+          <Suspense fallback={<div>Loading publish page...</div>}>
+            <Publish />
+          </Suspense>
+        ),
       },
     ],
   },
   {
     path: "/login",
-    element: <Login></Login>,
+    element: (
+      <Suspense fallback={<div>Loading login...</div>}>
+        <Login />
+      </Suspense>
+    ),
   },
 ]);
 
